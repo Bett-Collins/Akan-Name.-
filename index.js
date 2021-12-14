06c78854c6f6a96538b7f527f4e4e1098c7a5503
Bett-C <collins.kkiprotich.student@moringaschool.com>
2021-12-14 09:13:39 +0300

    it will add a function to validate the month

--- index.js
+++ index.js
@@ -7,5 +7,28 @@ function onSubmit() {
     var akanName = determineName(GenderEntered, calculatedDay);
     document.getElementById("Display").innerHTML = "hi! Your Akan Name Is " + akanName;
   
-  }
-  
+  };
+  function validDate() {
+    var date = document.getElementById("Date").value;
+    if (date) {
+        if (date <=0 || date > 31) {
+            return fulse;
+        } else {
+            alert("return date");
+        }
+    } else {
+        alert("please enter date")
+    }
+}
+function valiMonth() {
+    var month = document.getElementById("Month").value;
+    if (month) {
+        if ( month <=0 || month > 12) {
+            return fulse;
+        } else {
+            alert("return month");
+        }
+    } else {
+        alert("please enter month")
+    }
+}
